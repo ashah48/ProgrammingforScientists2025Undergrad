92613d3569b3e8125ff69058ae1bcb8069631531
Aahan Shah <aahanshah@macbook-pro-136.wifi.local.cmu.edu>
2025-10-09 09:24:46 -0400

    Starter Code

--- Starter Code/gray-scott/functions.py
+++ Starter Code/gray-scott/functions.py
@@ -1,38 +1,29 @@
 # Insert your simulate_gray_scott() function here, along with any subroutines that you need.
 import math
-from .datatypes import Board, Cell
+#from .datatypes import Board, Cell
 
+#HW 3 9.1
 def diffuse_board_one_particle(current_board: list[list[float]], kernel: list[list[float]]) -> list[list[float]]:
     """
     Input: A two-dimensional array of decimal numbers current_board and a 3 × 3 matrix of decimal numbers kernel.
     Output: A two-dimensional array of decimal numbers new_board corresponding to applying
     one time step of diffusion to current_board using the kernel kernel.
     """
+    concentrations = [[] * len(current_board)]
+    moores = make_moores(current_board)
+    for i in range(len(current_board)):
+        for j in range(len(current_board[i])):
+            concentrations[i][j] = convolution(moores[i][j], kernel)
+
+    return concentrations
 
 
 
+def convolution(neighborhood: list[list[float]], kernel: list[list[float]]) -> float:
+    pass
 
-def find_in_field(current_board: list[list[float]], row: int, col: int) -> list[tuple[int, int]]:
-    tuples_list = []
-    for r in range(len(current_board)):
-        for c in range(len(current_board[r])):
-            if in_field(current_board, row, col):
-                tuples_list.append(tuple[row, col])
+def make_moores(current_board: list[list[float]]) -> list[list[list[float]]]:
 
-    return tuples_list
-
-def make_field_for_cell(current_board: list[list[float]], row: int, col: int) -> list[list[float]]:
-    num_rows = len(current_board)
-    specific_board = []
-    for i in range(num_rows):
-        specific_board.append([])
-        if in_field(current_board, row - i, col - i):
-            specific_board[i].append(current_board[row-i][col-i])
-        elif in_field(current_board, row - i, col + i):
-            specific_board[i].append(current_board[row-i][col+i])
-        elif in_field(current_board, row + i, col - i):
-            specific_board[i].append(current_board[row-i][col+i])
-        elif in_field(current_board)
 
 
 def in_field(current_board: list[list[float]], row: int, col: int) -> bool:
